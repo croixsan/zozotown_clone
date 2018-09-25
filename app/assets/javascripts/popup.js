2218ca4ce56c1761f2713b4dfb3d74dbdcdcd198
suguru_nakayama <croixdusudx@gmail.com>
2018-09-25 17:09:23 +0900

    メニューボタン、カートボタンのポップアップモーション追加

--- app/assets/javascripts/popup.js
+++ app/assets/javascripts/popup.js
@@ -1,18 +1,56 @@
 $(function(){
-  // お気に入りボタンの上にカーソルが乗っている場合はポップアップの表示をする
+  //---------------------- header ----------------------------
+  //---- お気に入りボタン---------------------------
   $('#favorite-btn').on('mouseover', function(){
+    $("#header__wrapper__right__popup__inner__favorite").css("visibility","visible")
     $("#header__wrapper__right__popup").css("visibility","visible")
   })
-  // お気に入りボタンからカーソルが離れたときにポップアップを消す
   $('#favorite-btn').on('mouseout', function(){
+    $("#header__wrapper__right__popup__inner__favorite").css("visibility","hidden")
     $("#header__wrapper__right__popup").css("visibility","hidden")
   })
   // ポップアップの上にカーソルが乗っている場合はポップアップの表示を続ける
-  $('#header__wrapper__right__popup').on('mouseover', function(){
+  $('#header').on('mouseover', "#header__wrapper__right__popup__inner__favorite", function(){
+    $("#header__wrapper__right__popup__inner__favorite").css("visibility","visible")
     $("#header__wrapper__right__popup").css("visibility","visible")
   })
   // ポップアップからカーソルが離れたときにポップアップを消す
-  $('#header__wrapper__right__popup').on('mouseout', function(){
+  $('#header__wrapper__right__popup__inner__favorite').on('mouseout', function(){
+    $("#header__wrapper__right__popup__inner__favorite").css("visibility","hidden")
+    $("#header__wrapper__right__popup").css("visibility","hidden")
+  })
+  //---- カートボタン---------------------------
+  $('#cart-btn').on('mouseover', function(){
+    $("#header__wrapper__right__popup__inner__cart").css("visibility","visible")
+    $("#header__wrapper__right__popup").css("visibility","visible")
+  })
+  $('#cart-btn').on('mouseout', function(){
+    $("#header__wrapper__right__popup__inner__cart").css("visibility","hidden")
+    $("#header__wrapper__right__popup").css("visibility","hidden")
+  })
+  $('#header__wrapper__right__popup__inner__cart').on('mouseover', function(){
+    $("#header__wrapper__right__popup__inner__cart").css("visibility","visible")
+  })
+  $('#header__wrapper__right__popup__inner__cart').on('mouseout', function(){
+    $("#header__wrapper__right__popup__inner__cart").css("visibility","hidden")
+    $("#header__wrapper__right__popup").css("visibility","hidden")
+  })
+
+  //---- メニューボタン---------------------------
+  $('#menu-btn').on('mouseover', function(){
+    $("#header__wrapper__right__popup__inner__menu").css("visibility","visible")
+    $("#header__wrapper__right__popup").css("visibility","visible")
+  })
+  $('#menu-btn').on('mouseout', function(){
+    $("#header__wrapper__right__popup__inner__menu").css("visibility","hidden")
+    $("#header__wrapper__right__popup").css("visibility","hidden")
+  })
+  $('#header__wrapper__right__popup__inner__menu').on('mouseover', function(){
+    $("#header__wrapper__right__popup__inner__menu").css("visibility","visible")
+    $("#header__wrapper__right__popup").css("visibility","visible")
+  })
+  $('#header__wrapper__right__popup__inner__menu').on('mouseout', function(){
+    $("#header__wrapper__right__popup__inner__menu").css("visibility","hidden")
     $("#header__wrapper__right__popup").css("visibility","hidden")
   })
 })
