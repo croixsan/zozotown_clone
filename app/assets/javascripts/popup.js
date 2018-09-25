ad0aabfc8aba404b014c5a6ffc40dfc6ba7e3c7d
suguru_nakayama <croixdusudx@gmail.com>
2018-09-25 17:47:28 +0900

    可読性向上のため、クラス名変更

--- app/assets/javascripts/popup.js
+++ app/assets/javascripts/popup.js
@@ -2,55 +2,56 @@ $(function(){
   //---------------------- header ----------------------------
   //---- お気に入りボタン---------------------------
   $('#favorite-btn').on('mouseover', function(){
-    $("#header__wrapper__right__popup__inner__favorite").css("visibility","visible")
-    $("#header__wrapper__right__popup").css("visibility","visible")
+    $(".popup__inner__favorite").css("visibility","visible")
+    $(".popup").css("visibility","visible")
   })
   $('#favorite-btn').on('mouseout', function(){
-    $("#header__wrapper__right__popup__inner__favorite").css("visibility","hidden")
-    $("#header__wrapper__right__popup").css("visibility","hidden")
+    $(".popup__inner__favorite").css("visibility","hidden")
+    $(".popup").css("visibility","hidden")
   })
   // ポップアップの上にカーソルが乗っている場合はポップアップの表示を続ける
-  $('#header').on('mouseover', "#header__wrapper__right__popup__inner__favorite", function(){
-    $("#header__wrapper__right__popup__inner__favorite").css("visibility","visible")
-    $("#header__wrapper__right__popup").css("visibility","visible")
+  $(".popup__inner__favorite").on('mouseover', function(){
+    $(".popup__inner__favorite").css("visibility","visible")
+    $(".popup").css("visibility","visible")
   })
   // ポップアップからカーソルが離れたときにポップアップを消す
-  $('#header__wrapper__right__popup__inner__favorite').on('mouseout', function(){
-    $("#header__wrapper__right__popup__inner__favorite").css("visibility","hidden")
-    $("#header__wrapper__right__popup").css("visibility","hidden")
+  $(".popup__inner__favorite").on('mouseout', function(){
+    $(".popup__inner__favorite").css("visibility","hidden")
+    $(".popup").css("visibility","hidden")
   })
   //---- カートボタン---------------------------
   $('#cart-btn').on('mouseover', function(){
-    $("#header__wrapper__right__popup__inner__cart").css("visibility","visible")
-    $("#header__wrapper__right__popup").css("visibility","visible")
+    $(".popup__inner__cart").css("visibility","visible")
+    $(".popup").css("visibility","visible")
   })
   $('#cart-btn').on('mouseout', function(){
-    $("#header__wrapper__right__popup__inner__cart").css("visibility","hidden")
-    $("#header__wrapper__right__popup").css("visibility","hidden")
+    $(".popup__inner__cart").css("visibility","hidden")
+    $(".popup").css("visibility","hidden")
   })
-  $('#header__wrapper__right__popup__inner__cart').on('mouseover', function(){
-    $("#header__wrapper__right__popup__inner__cart").css("visibility","visible")
+  $(".popup__inner__cart").on('mouseover', function(){
+    $(".popup__inner__cart").css("visibility","visible")
+    $(".popup").css("visibility","visible")
   })
-  $('#header__wrapper__right__popup__inner__cart').on('mouseout', function(){
-    $("#header__wrapper__right__popup__inner__cart").css("visibility","hidden")
-    $("#header__wrapper__right__popup").css("visibility","hidden")
+  $(".popup__inner__cart").on('mouseout', function(){
+    $(".popup__inner__cart").css("visibility","hidden")
+    $(".popup").css("visibility","hidden")
   })
 
   //---- メニューボタン---------------------------
   $('#menu-btn').on('mouseover', function(){
-    $("#header__wrapper__right__popup__inner__menu").css("visibility","visible")
-    $("#header__wrapper__right__popup").css("visibility","visible")
+    $(".popup__inner__menu").css("visibility","visible")
+    $(".popup").css("visibility","visible")
   })
   $('#menu-btn').on('mouseout', function(){
-    $("#header__wrapper__right__popup__inner__menu").css("visibility","hidden")
-    $("#header__wrapper__right__popup").css("visibility","hidden")
+    $(".popup__inner__menu").css("visibility","hidden")
+    $(".popup").css("visibility","hidden")
   })
-  $('#header__wrapper__right__popup__inner__menu').on('mouseover', function(){
-    $("#header__wrapper__right__popup__inner__menu").css("visibility","visible")
-    $("#header__wrapper__right__popup").css("visibility","visible")
+  $('.popup__inner__menu').on('mouseover', function(){
+    $(".popup__inner__menu").css("visibility","visible")
+    $(".popup").css("visibility","visible")
   })
-  $('#header__wrapper__right__popup__inner__menu').on('mouseout', function(){
-    $("#header__wrapper__right__popup__inner__menu").css("visibility","hidden")
-    $("#header__wrapper__right__popup").css("visibility","hidden")
+  $('.popup__inner__menu').on('mouseout', function(){
+    $(".popup__inner__menu").css("visibility","hidden")
+    $(".popup").css("visibility","hidden")
   })
 })
