a61f89beadfbc1e9862b200e485d0453c5fe7c94
yutahorii <iu25c-sonium32@live.jp>
2018-09-26 19:04:09 +0900

    Edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "zozotown"
-set :repo_url, "git@example.com:Suguru-Nakayama/zozotown.git"
+set :repo_url, "git@github.com:Suguru-Nakayama/zozotown.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
