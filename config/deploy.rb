863e1e3e87ec42956ddd596a8043d6bb34e3b8ca
yutahorii <iu25c-sonium32@live.jp>
2018-09-26 18:52:11 +0900

    add_capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "zozotown"
+set :repo_url, "git@example.com:Suguru-Nakayama/zozotown.git"
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['/Users/iu25c/.ssh/key_zozotown.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '13.114.9.54', user: 'ec2-user', roles: %w{app db web}
