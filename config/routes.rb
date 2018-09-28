29a2b48e5e776795b1e06b00c6c8ea446e264deb
suguru_nakayama <croixdusudx@gmail.com>
2018-09-28 16:47:02 +0900

    top shopのルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  root 'tests#index'
+  root 'tops#index'
   get 'tests/new' => 'tests#new'
   get 'carts/index' => 'carts#index'
+
+
+  get "men_top"    => "tops#men"
+  get "women_top"  => "tops#women"
+  get "kid_top"    => "tops#kid"
+
+  resources :men_shops, only: [:index, :show]
+  resources :women_shops, only: [:index, :show]
+  resources :kid_shops, only: [:index, :show]
+  resources :shops, only: [:index, :show] do
+    resources :items, only: [:index, :show]
+  end
 end
