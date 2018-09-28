51e7096bb57fc93cd64e3510f8a6762701fb611f
yutahorii <iu25c-sonium32@live.jp>
2018-09-28 14:16:18 +0900

    edit_migrate&edit_app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up, keys:[:name], [:gender], [:post_num], [:address], [:phone_num] )
+  end
 end
