570e14cf35f33798d3320f72f6df16e9fde0d8df
suguru_nakayama <croixdusudx@gmail.com>
2018-09-28 21:18:15 +0900

    Brandモデルアソシエーション定義

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,5 @@
 class Brand < ApplicationRecord
+  has_many :items
+  has_many :favorite_brands
+  enum gender: {all: 1, mens: 2, ladies: 3, kids: 4}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   enum gender: { men: 1, women: 2, other: 3 }
 
+  has_one :cart, dependent: :destroy
+  has_many :checked_items
+  has_many :checked_shops
+  has_many :favorite_items
+  has_many :favorite_brands
+  has_many :favorite_shops
+
 end
