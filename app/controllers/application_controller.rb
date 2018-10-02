a8cf481daf3b7af534d423f0f31c5a9aad960240
suguru_nakayama <croixdusudx@gmail.com>
2018-10-02 14:40:29 +0900

    サインアップ時のパラメーターとして誕生日を許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_num])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_year])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_month])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_day])
   end
 end
