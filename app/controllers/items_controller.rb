bd50c44a3f89bdf0d940a200c81b18f16607184e
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-03 19:07:42 +0900

    Item-controller edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,9 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @cart = Cart.find_by(user_id: 1)
+    #1はcurrent_user.idにする
+    @items = Item.find(params[:id])
+    @images = Image.find(params[:id])
   end
 end
