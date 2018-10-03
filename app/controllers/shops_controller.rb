e8a0660340b43fc97e69ddc8464542b7715ce551
suguru_nakayama <croixdusudx@gmail.com>
2018-10-03 15:16:01 +0900

    Shop一覧ページの作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,10 @@
 class ShopsController < ApplicationController
   def index
+    @shops = []
+    # アルファベットで始まるショップの検索
+    ("A".."Z").each do |initial|
+      @shops << Shop.where("name LIKE(?)", "#{initial}%").includes(:items)
+    end
   end
 
   def show
