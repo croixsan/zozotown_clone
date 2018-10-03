6fca8c0f4c8b42cfe4328a2318cda385f6a7f3d2
suguru_nakayama <croixdusudx@gmail.com>
2018-10-03 11:09:18 +0900

    イニシャル順でのBrandの表示を実装

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,4 +1,9 @@
 class BrandsController < ApplicationController
   def index
+    @brands = []
+    # アルファベットで始まるショップの検索
+    ("A".."Z").each_with_index do |initial, i|
+      @brands << Brand.where("name LIKE(?)", "#{initial}%").includes(:items)
+    end
   end
 end
