fa038a53b58ff55966f9dd7fea4737997e66e9b9
suguru_nakayama <croixdusudx@gmail.com>
2018-10-03 12:46:50 +0900

    each_with_index文をeach文に変更

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,7 +2,7 @@ class BrandsController < ApplicationController
   def index
     @brands = []
     # アルファベットで始まるショップの検索
-    ("A".."Z").each_with_index do |initial, i|
+    ("A".."Z").each do |initial|
       @brands << Brand.where("name LIKE(?)", "#{initial}%").includes(:items)
     end
   end
