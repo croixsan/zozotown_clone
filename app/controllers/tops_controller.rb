fca53ad862de3c876bd88f8ca4fe7b4f3783b967
suguru_nakayama <croixdusudx@gmail.com>
2018-10-05 21:12:38 +0900

    N+1対策

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,17 +1,13 @@
 class TopsController < ApplicationController
   def index
     @zozo_brand = Brand.find_by(name: "ZOZO")
-    @zozo_items = @zozo_brand.items.limit(3)
+    @zozo_items = @zozo_brand.items.limit(3).includes(:images)
     @coupon = Coupon.find_by(price: 5000)
-    @coupon_items = @coupon.items.limit(7)
-    @rank_items = Item.order("created_at DESC").limit(7)
-    @checked_items = Item.order("created_at DESC").limit(15)
+    @coupon_items = @coupon.items.limit(7).includes(:images)
+    @rank_items = Item.order("created_at DESC").limit(7).includes(:images)
+    @checked_items = Item.order("created_at DESC").limit(15).includes(:images)
     @top_categories = TopCategory.all
     @brands = Brand.order("created_at DESC").limit(10)
-
-    @shop_images = ["https://img4.zozo.jp/common/shop/shop_1982.jpg", "https://img4.zozo.jp/common/shop/shop_752.jpg", "https://img4.zozo.jp/common/shop/shop_361.jpg", "https://img4.zozo.jp/common/shop/shop_273.jpg", "https://img4.zozo.jp/common/shop/shop_1320.jpg",
-    "https://img4.zozo.jp/common/shop/shop_100.jpg", "https://img4.zozo.jp/common/shop/shop_1889.jpg", "https://img4.zozo.jp/common/shop/shop_71.jpg", "https://img4.zozo.jp/common/shop/shop_197.jpg", "https://img4.zozo.jp/common/shop/shop_1144.jpg", "https://img4.zozo.jp/common/shop/shop_72.jpg"]
-
   end
 
   def men
