f816c37bfa1fbe0939e06205481134f139385c2b
suguru_nakayama <croixdusudx@gmail.com>
2018-10-06 21:19:13 +0900

    アイテム詳細ページを閲覧した時にchecked_itemsテーブルにレコードを保存する処理を追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,16 @@ class ItemsController < ApplicationController
     @related_items = Item.where(shop_id: @item.shop.id)
     @color_count = @item.images.group(:color).length - 1 # -1: nilを除外
     @stock_count = @item.stocks.length
+
+    # 「チェックしたアイテム」機能
+    checked_items = current_user.checked_items
+    # 同じアイテムを過去に閲覧していなければレコード作成
+    if checked_items.find_by(item_id: @item.id) == nil
+      checked_items.create(item_id: @item.id)
+    end
+    if checked_items.length > 20
+      checked_items.first.destroy
+    end
+
   end
 end
