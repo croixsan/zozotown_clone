359598dbaed25a1fb67ea792359a87ceada83023
suguru_nakayama <croixdusudx@gmail.com>
2018-10-06 14:59:34 +0900

    トップページの表示内容変更

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,8 +2,8 @@ class TopsController < ApplicationController
   def index
     @zozo_brand = Brand.find_by(name: "ZOZO")
     @zozo_items = @zozo_brand.items.limit(3).includes(:images)
-    @coupon = Coupon.find_by(price: 5000)
-    @coupon_items = @coupon.items.limit(7).includes(:images)
+    # @coupon = Coupon.find_by(price: 5000)
+    @coupon_items = Item.order("created_at DESC").limit(7)
     @rank_items = Item.order("created_at DESC").limit(7).includes(:images)
     @checked_items = Item.order("created_at DESC").limit(15).includes(:images)
     @top_categories = TopCategory.all
