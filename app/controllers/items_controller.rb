f125d4e009cdcc3fae09742c0ac34e817b44e845
suguru_nakayama <croixdusudx@gmail.com>
2018-10-06 14:59:18 +0900

    アイテム詳細ページのデータベース連携

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,9 @@ class ItemsController < ApplicationController
   def show
     @cart = current_user.cart
     @item = Item.find(params[:id])
-    @images = Image.find(params[:id])
+    @images = @item.images
+    @related_items = Item.where(shop_id: @item.shop.id)
+    @color_count = @item.images.group(:color).length - 1 # -1: nilを除外
+    @stock_count = @item.stocks.length
   end
 end
