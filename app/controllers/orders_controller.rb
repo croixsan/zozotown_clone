518297bab2bbf1270451191a13f05293f55d666f
yutahorii <iu25c-sonium32@live.jp>
2018-10-07 16:31:37 +0900

    Haml,css_create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,11 +5,17 @@ class OrdersController < ApplicationController
 
   def select
     @derivery = Delivery.new
+    @orders = Order.new
+    @cards = Card.new
   end
 
   def registration
     @derivery = Delivery.new
     @delivery.save
+    @orders = Order.new
+    @orders.save
+    @cards = Card.new
+    @card.save
     redirect_to orders_select
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "order#select"
+  root "orders#select"
   # root "tops#index"
   devise_for :users, controllers: {
     registrations: 'users/registrations',
