ac90efa8bb58d0783e08d569bd17f90c1ba1add6
suguru_nakayama <croixdusudx@gmail.com>
2018-10-07 21:00:50 +0900

    コメントアウトされていたアソシエーションを解除

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   # has_many :ordered_items
   # has_many :orders, through: :ordered_items
   has_many :checked_items, dependent: :destroy
-  # has_many :favorite_items
+  has_many :favorite_items
 
   # enum gender: {all: 1, mens: 2, ladies: 3, kids: 4}
 end
