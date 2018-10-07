da559beddb10655deff860e1cd9b6077c40b8f86
suguru_nakayama <croixdusudx@gmail.com>
2018-10-07 18:32:13 +0900

    チェックしたアイテムの追加のロジック変更

--- app/controllers/concerns/checked.rb
+++ app/controllers/concerns/checked.rb
@@ -3,8 +3,8 @@ module Checked
   included do
     def get_checked_items
       # 以前閲覧したアイテムを取得する
-      checked_items = current_user.checked_items.order("created_at DESC").map do |checked_item|
-         Item.includes(:images).find_by(id: checked_item.item_id)
+      checked_items = current_user.checked_items.order("created_at DESC").group(:item_id).map do |checked_item|
+        Item.includes(:images).find_by(id: checked_item.item_id)
       end
       return checked_items
     end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,15 +11,6 @@ class ItemsController < ApplicationController
     @stock_count = @item.stocks.length
 
     # 「チェックしたアイテム」機能
-    checked_item = current_user.checked_items.find_by(item_id: @item.id)
-    # 既にアイテムを閲覧していれば既存のレコードを削除
-    checked_item.destroy if checked_item
     current_user.checked_items.create(item_id: @item.id)
-
-    # チェックしたアイテムが規定数を超えた場合、古いレコードを削除
-    if current_user.checked_items.length > 20
-      checked_items.first.destroy
-    end
-
   end
 end
