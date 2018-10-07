c73479622f27a21bfdced2e1d251ff34114e3a8d
suguru_nakayama <croixdusudx@gmail.com>
2018-10-08 00:45:43 +0900

    favoritesコントローラーのcreate, destroyアクション定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :set_render_parameter
+
   def create
+    current_user.favorite_items.create(item_num_id: params[:item_num_id])
   end
 
   def destroy
+    favorite_items = current_user.favorite_items
+    favorite_items.find_by(item_num_id: params[:item_num_id]).destroy()
+  end
+
+  private
+  def set_render_parameter
+    @item = Item.find(params[:item_id])
+    @shop = @item.shop
+    @item_num_id = params[:item_num_id]
   end
 end
