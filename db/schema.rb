bd2f0ce0c19298a91ba7eac549423f28b37883d2
suguru_nakayama <croixdusudx@gmail.com>
2018-10-09 08:41:41 +0900

    masterからpull

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181008112757) do
+ActiveRecord::Schema.define(version: 20181008113750) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",        null: false
@@ -136,13 +136,13 @@ ActiveRecord::Schema.define(version: 20181008112757) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "cart_id",       null: false
-    t.integer  "delivery_id",   null: false
     t.datetime "buy_date"
     t.integer  "card_id",       null: false
+    t.integer  "delivery_id",   null: false
     t.integer  "delivery_kind", null: false
     t.string   "delivery_day"
     t.string   "delivery_hour"
-    t.integer  "payment_id"
+    t.integer  "payment_id",    null: false
   end
 
   create_table "payments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
