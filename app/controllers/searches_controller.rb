1bdc8295d1f549dd558c7a4d670c563d2d27daf7
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-08 17:46:23 +0900

    Add index-controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,11 @@
 class SearchesController < ApplicationController
 
   def index
+    @brands = Brand.where('title LIKE(?)', "a").limit(20)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
 end
