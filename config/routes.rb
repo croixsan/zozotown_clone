c0f00c6fbe438d7c24db5810efd67ca4b8e9f1b1
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-08 14:47:35 +0900

    Add routing-search

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   get 'rankings/index' => 'rankings#index'
 
+  get 'searches/index' => 'searches#index'
+
   get "men_top"    => "tops#men"
   get "women_top"  => "tops#women"
   get "kid_top"    => "tops#kid"
