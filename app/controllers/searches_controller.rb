376b1a46337ebdfca7ca583149ce1f0fcdf67e0b
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-09 20:10:49 +0900

    add to shops items top_categories sub_categories

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,13 @@
 class SearchesController < ApplicationController
 
   def index
-    @brands = Brand.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+    @brands = Brand.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    @shops = Shop.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    @top_categories = TopCategory.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    @sub_categories = SubCategory.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    @searches = @brands + @shops + @items + @top_categories + @sub_categories
+
     respond_to do |format|
       format.html
       format.json
