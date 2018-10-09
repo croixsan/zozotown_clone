9b2e59fddcde1f091a74905e4afdc21638af0b33
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-09 21:34:06 +0900

    Add to order-method

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,11 @@
 class SearchesController < ApplicationController
 
   def index
-    @brands = Brand.where('name LIKE(?)', "%#{params[:keyword]}%").limit(3)
-    @shops = Shop.where('name LIKE(?)', "%#{params[:keyword]}%").limit(3)
-    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").limit(3)
-    @top_categories = TopCategory.where('name LIKE(?)', "%#{params[:keyword]}%").limit(3)
-    @sub_categories = SubCategory.where('name LIKE(?)', "%#{params[:keyword]}%").limit(3)
+    @brands = Brand.where('name LIKE(?)', "%#{params[:keyword]}%").order('id').limit(3)
+    @shops = Shop.where('name LIKE(?)', "%#{params[:keyword]}%").order('id').limit(3)
+    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").order('id').limit(3)
+    @top_categories = TopCategory.where('name LIKE(?)', "%#{params[:keyword]}%").order('id').limit(3)
+    @sub_categories = SubCategory.where('name LIKE(?)', "%#{params[:keyword]}%").order('id').limit(3)
     @searches = @brands + @shops + @items + @top_categories + @sub_categories
 
     respond_to do |format|
