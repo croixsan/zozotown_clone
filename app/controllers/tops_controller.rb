6166719d73be5888f658e1992ae7064f75d0b3a3
suguru_nakayama <croixdusudx@gmail.com>
2018-10-09 19:01:01 +0900

    トップページにランキングを表示

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,11 +1,11 @@
 class TopsController < ApplicationController
   include Checked
+  include Ranking
   def index
     @zozo_brand = Brand.find_by(name: "ZOZO")
     @zozo_items = @zozo_brand.items.limit(3).includes(:images)
     # @coupon = Coupon.find_by(price: 5000)
     @coupon_items = Item.order("created_at DESC").limit(7)
-    @rank_items = Item.order("created_at DESC").limit(7).includes(:images)
     @top_categories = TopCategory.all
     @brands = Brand.order("items_count DESC").limit(10)
     @shops = Shop.order("items_count DESC").limit(10)
@@ -13,6 +13,9 @@ class TopsController < ApplicationController
     @checked_items = get_checked_items
     # チェックしたショップ
     @checked_shops = get_checked_shops
+
+    @rankings = get_ranking_items.slice(0, 23)
+
   end
 
   def men
