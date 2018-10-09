8c0fffdd60f1a5c67de0dc1bedcb07a89e358f6d
suguru_nakayama <croixdusudx@gmail.com>
2018-10-09 12:05:20 +0900

    user_idにindexオプションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009004916) do
+ActiveRecord::Schema.define(version: 20181009030248) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",        null: false
@@ -152,6 +152,7 @@ ActiveRecord::Schema.define(version: 20181009004916) do
     t.integer  "item_num_id", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_past_carts_on_user_id", using: :btree
   end
 
   create_table "payments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
