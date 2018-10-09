0e56a237ef169ac0f3b59ddcbdc4266b711f729a
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-09 20:40:20 +0900

    Edit incremental error to .fail

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -23,15 +23,14 @@ var search_list = $("#header__wrapper__left__search__incremental__area");
       data: { keyword: input },
       dataType: 'json'
     })
-   .done(function(searches) {
-      console.log(searches)
+    .done(function(searches) {
       $("#header__wrapper__left__search__incremental__area").empty();
         searches.forEach(function(search){
           appendProduct(search);
       });
     })
-    // .fail(function() {
-    //   alert('映画検索に失敗しました');
-    // })
+    .fail(function() {
+      alert('映画検索に失敗しました');
+    })
   });
 });
