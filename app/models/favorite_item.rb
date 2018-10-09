79631b704e07acd458af96f737bb724317e35131
suguru_nakayama <croixdusudx@gmail.com>
2018-10-09 15:02:20 +0900

    counter_cacheアソシエーションを追加

--- app/models/favorite_item.rb
+++ app/models/favorite_item.rb
@@ -1,4 +1,5 @@
 class FavoriteItem < ApplicationRecord
   belongs_to :user
   belongs_to :item_num
+  belongs_to :item, counter_cache: :favorite_items_count
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,11 @@ class Item < ApplicationRecord
   has_many :stocks
   has_many :images
   has_many :item_nums
-  # has_many :shoppings, dependent: :destroy
+  has_many :shoppings, dependent: :destroy
   has_many :carts, through: :shoppings
-  # has_many :ordered_items
+  has_many :ordered_items
   # has_many :orders, through: :ordered_items
   has_many :checked_items, dependent: :destroy
+  has_many :favorite_items
   # enum gender: {all: 1, mens: 2, ladies: 3, kids: 4}
 end
