850b9ee73e7ebe0284119c4ac478600691bcdedf
suguru_nakayama <croixdusudx@gmail.com>
2018-10-10 20:39:01 +0900

    日時指定のText boxを途中まで作成

---
+++ app/assets/javascripts/pre_order.js
@@ -0,0 +1,16 @@
+$(function(){
+  // function buildHtml(){
+  //   var html = `
+  //   <input type="text" class="input_day">
+  //   <input type="text" class="input_hour">
+  //   `
+  //   return html
+  // }
+  // $('.delivery_select').on('click', function(){
+  //   console.log($(this).attr("id"))
+  //   if ($(this).attr("id") == "delivery_date"){
+  //     html = buildHtml();
+  //     $("#select_" + $(this).attr("data-id")).append(html)
+  //   }
+  // })
+})
