bfd7e8f85f51747414cf20243b5bd51bc4c5934d
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-10 21:48:54 +0900

    Add mouse out remove class method

--- app/assets/javascripts/enlargement.js
+++ app/assets/javascripts/enlargement.js
@@ -2,4 +2,7 @@ $(function() {
   $(".main-image").on("mousemove", function(){
     $(".zoom-area").addClass('active');
   });
+  $(".main-image").on("mouseout", function(){
+    $(".zoom-area").removeClass('active');
+  });
 });
