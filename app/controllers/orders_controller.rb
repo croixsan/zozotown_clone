2716e4bf52047728aa3c1a9aae03c507781641c6
suguru_nakayama <croixdusudx@gmail.com>
2018-10-11 10:37:23 +0900

    コントローラー作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,22 @@
 class OrdersController < ApplicationController
   before_action :setup_user
   def index
+    @pre_order = current_user.pre_order
+    @cart = current_user.cart
+    @item_nums = current_user.cart.item_nums.group(:number)
+    @counts = @item_nums.count
+    # 商品合計金額、送料、手数料から最終金額を算出
+    @total_price = @cart.total_price + @pre_order.delivery.price + @pre_order.payment.price
+
+    # 利用可能なクーポンの算出（アイテムに１つでもクーポン対象商品が含まれていれば、クーポン利用可能とする）
+    @coupon = 0
+    @cart.items.each do |item|
+      if item.coupon.price > 0
+        @coupon = item.coupon.price
+        break
+      end
+    end
+    @discount_point = current_user.point + @coupon
   end
 
   def select
@@ -42,7 +58,7 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(
         :delivery_kind,
-        :delivery_day, 
+        :delivery_day,
         :delivery_hour
         ).merge(cart_id: current_user.cart.id, user_id: current_user.id, delivery_id: @delivery.id)
     end
