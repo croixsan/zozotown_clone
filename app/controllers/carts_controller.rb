f8004ec1272d949ae4ca47c9255d61ba55360fff
suguru_nakayama <croixdusudx@gmail.com>
2018-10-11 10:34:00 +0900

    カート一覧画面でカート内の合計金額をupdateする処理を追加。以前カートに入れたアイテムの処理をリダイレクト前に行うように修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,7 +8,9 @@ class CartsController < ApplicationController
     # カートに含まれる同じアイテムをアイテム番号毎にグループ化
     @item_nums = @cart.item_nums.group(:number)
     @count = @item_nums.count
+    # カート内の合計金額を算出
     @total_price = get_total_price(@items)
+    @cart.update(total_price: @total_price)
     # チェックしたアイテム
     @checked_items = get_checked_items
 
@@ -18,16 +20,17 @@ class CartsController < ApplicationController
 
   def create
     Shopping.create(cart_params)
-    redirect_to controller: 'carts', action: 'index'
 
     # 「以前カートに入れたアイテム」機能
     current_user.past_carts.where(item_num_id: params[:item_num_id]).first_or_create.update(updated_at: Time.current)
+    
+    redirect_to controller: 'carts', action: 'index'
   end
 
   def destroy
     if params[:id]
-      shopping = Shopping.where(item_id: params[:id])
-      shopping.destroy_all
+      shoppings = Shopping.where(item_id: params[:id])
+      shoppings.destroy_all
     elsif params[:item_id]
       shopping = Shopping.find_by(item_id: params[:item_id])
       shopping.destroy
