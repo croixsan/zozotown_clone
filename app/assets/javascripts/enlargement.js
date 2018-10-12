2362aa5f8d9c913b37024b07d15258872bb6c306
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-12 21:32:57 +0900

    Add mouse move-offset

--- app/assets/javascripts/enlargement.js
+++ app/assets/javascripts/enlargement.js
@@ -1,8 +1,24 @@
 $(function() {
   $(".main-image").on("mousemove", function(){
     $(".zoom-area").addClass('active');
+    $(".image-grass").addClass('active');
+
+      $('.main-image').on('mousemove', function(e) {
+        var left = e.offsetX + 'px';
+        var top = e.offsetY + 'px';
+        console.log(top)
+        $( ".image-grass" ).css( "margin-top" , top );
+        $( ".image-grass" ).css( "margin-left" , left );
+        var unleft = "-" + e.offsetX + 'px';
+        var untop = "-" + e.offsetY + 'px';
+        $( ".zoom_image" ).css( "margin-top" , untop );
+        $( ".zoom_image" ).css( "margin-left" , unleft );
+
+
+      });
   });
   $(".main-image").on("mouseout", function(){
     $(".zoom-area").removeClass('active');
   });
 });
+
