2e23d93e588da93a345ee1a37b2cf77f809a527c
suguru_nakayama <croixdusudx@gmail.com>
2018-10-12 10:55:40 +0900

    希望日時の選択フォームを出現させるjavascliptを追加

--- app/assets/javascripts/pre_order.js
+++ app/assets/javascripts/pre_order.js
@@ -1,16 +1,25 @@
 $(function(){
-  // function buildHtml(){
-  //   var html = `
-  //   <input type="text" class="input_day">
-  //   <input type="text" class="input_hour">
-  //   `
-  //   return html
-  // }
-  // $('.delivery_select').on('click', function(){
-  //   console.log($(this).attr("id"))
-  //   if ($(this).attr("id") == "delivery_date"){
-  //     html = buildHtml();
-  //     $("#select_" + $(this).attr("data-id")).append(html)
-  //   }
-  // })
+  function buildHtml(){
+    var html = `
+    <input type="text" class="input_day">
+    <input type="text" class="input_hour">
+    `
+    return html
+  }
+  $('#card_payment').on('click', function(){
+    $(".card_table").css("display","block")
+  })
+
+  $('.other_payment').on('click', function(){
+    $(".card_table").css("display","none")
+  })
+
+  $('.table_left').on('click', '#select_date', function(){
+    $(".select_form").css("display","block")
+  })
+
+  $('.table_left').on('click', '.other_date', function(){
+    $(".select_form").css("display","none")
+  })
+
 })
