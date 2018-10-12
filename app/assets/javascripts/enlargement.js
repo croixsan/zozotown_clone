32d77868faa6d8e2a11698bdcf9817199ac962bc
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-12 21:40:50 +0900

    Cursor center in image-grass

--- app/assets/javascripts/enlargement.js
+++ app/assets/javascripts/enlargement.js
@@ -4,17 +4,16 @@ $(function() {
     $(".image-grass").addClass('active');
 
       $('.main-image').on('mousemove', function(e) {
-        var left = e.offsetX + 'px';
-        var top = e.offsetY + 'px';
-        console.log(top)
+        var size_left = e.offsetX - 100;
+        var size_top = e.offsetY - 100;
+        var left = size_left + 'px';
+        var top = size_top + 'px';
         $( ".image-grass" ).css( "margin-top" , top );
         $( ".image-grass" ).css( "margin-left" , left );
         var unleft = "-" + e.offsetX + 'px';
         var untop = "-" + e.offsetY + 'px';
         $( ".zoom_image" ).css( "margin-top" , untop );
         $( ".zoom_image" ).css( "margin-left" , unleft );
-
-
       });
   });
   $(".main-image").on("mouseout", function(){
