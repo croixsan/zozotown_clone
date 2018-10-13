c0ff299585d3f51c372e5fb85e7bddbafb3717ee
suguru_nakayama <croixdusudx@gmail.com>
2018-10-13 17:13:00 +0900

    新着アイテムをトップページに表示。ゾゾアイテムを削除

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,22 +2,20 @@ class TopsController < ApplicationController
   include Checked
   include Ranking
   def index
-    @zozo_brand = Brand.find_by(name: "ZOZO")
-    @zozo_items = @zozo_brand.items.limit(3).includes(:images)
-    # @coupon = Coupon.find_by(price: 5000)
-    @coupon_items = Item.order("created_at DESC").limit(7)
+    @newest_items = Item.order("created_at DESC").includes(:images).limit(9)
+    @coupon_items = Item.order("created_at DESC").includes(:images).limit(9)
     @top_categories = TopCategory.all
     @brands = Brand.order("items_count DESC").limit(10)
     @shops = Shop.order("items_count DESC").limit(10)
 
     if user_signed_in?
       # チェックしたアイテム
-      @checked_items = get_checked_items
+      @checked_items = get_checked_items.slice(0, 18)
       # チェックしたショップ
-      @checked_shops = get_checked_shops
+      @checked_shops = get_checked_shops.slice(0, 8)
     end
 
-    @rankings = get_ranking_items.slice(0, 23)
+    @rankings = get_ranking_items.slice(0, 9)
 
   end
 
