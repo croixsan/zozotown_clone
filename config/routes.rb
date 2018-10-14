9fbea665cc659f9cf7083c0a9e4ed146dc8d8d3a
suguru_nakayama <croixdusudx@gmail.com>
2018-10-14 19:04:28 +0900

    クーポン用のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,25 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
   }
   get 'carts/index' => 'carts#index'
+  post 'carts/create' => 'carts#create'
+  delete 'carts/destroy' => 'carts#destroy'
+
 
   get 'carts/register' => 'carts#register'
 
-  post 'carts/create' => 'carts#create'
-
-  delete 'carts/destroy' => 'carts#destroy'
-
-  resources :rankings, only: :index
-
   get 'searches/index' => 'searches#index'
 
-  get "men_top"    => "tops#men"
-  get "women_top"  => "tops#women"
-  get "kid_top"    => "tops#kid"
-  get "tests/search" => "tests#search"
+  # -- クーポン機能 --------------------
+  resources :coupons, only: [:index, :create]
 
+  # -- ランキング機能 --------------------
+  resources :rankings, only: :index
+
+  # -- ショッピング機能 --------------------
   resources :carts, only: [:index, :create, :destroy, :show] do
+    collection do
+      get :register
+    end
   end
   resources :orders, only: [:index, :new, :create, :show]
   resources :pre_orders, only: [:new, :create]
@@ -31,20 +33,6 @@ Rails.application.routes.draw do
       get :shop
       get :delivery
       get :payment
-    end
-  end
-
-  concern :categories do
-    resources :top_categories, only: [:index, :show] do
-      resources :sub_categories, only: [:index, :show]
-    end
-  end
-
-  resources :tops, only: [:index]
-  resources :brands, only: [:index, :show], concerns: :categories
-  resources :shops, only: [:index, :show], concerns: :categories do
-    resources :items, only: [:index, :show] do
-      resources :favorites, only: [:create, :destroy]
     end
   end
 
@@ -60,8 +48,24 @@ Rails.application.routes.draw do
     end
   end
 
+  # -- カテゴリー --------------------
   resources :top_categories, only: [:index, :show] do
     resources :sub_categories, only: [:index, :show]
+  end
+
+  concern :categories do
+    resources :top_categories, only: [:index, :show] do
+      resources :sub_categories, only: [:index, :show]
+    end
+  end
+
+  # -- Base ------------------------
+  resources :tops, only: [:index]
+  resources :brands, only: [:index, :show], concerns: :categories
+  resources :shops, only: [:index, :show], concerns: :categories do
+    resources :items, only: [:index, :show] do
+      resources :favorites, only: [:create, :destroy]
+    end
   end
 
   scope :mens do
