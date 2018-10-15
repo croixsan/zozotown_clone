894118eb2d9593580fa0276ee99f4fda7fe7d35a
suguru_nakayama <croixdusudx@gmail.com>
2018-10-15 14:06:17 +0900

    couponsテーブルにshop_id追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181012113049) do
+ActiveRecord::Schema.define(version: 20181015050042) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",        null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20181012113049) do
     t.integer  "price",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "shop_id",    null: false
   end
 
   create_table "deliveries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
