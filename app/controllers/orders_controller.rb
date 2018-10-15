41b31b7f0f991f2ecc640ba91ba6fe9a93dda444
suguru_nakayama <croixdusudx@gmail.com>
2018-10-15 19:44:30 +0900

    カラム変更に伴うクーポン処理の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
     @total_price = @cart.total_price + @pre_order.delivery.price + @pre_order.payment.price
 
     # 利用可能なクーポンの算出（アイテムに１つでもクーポン対象商品が含まれていれば、クーポン利用可能とする）
+    @coupon = 0
     @cart.items.each do |item|
       if @coupon = item.shop.coupon
         break;
@@ -25,16 +26,16 @@ class OrdersController < ApplicationController
   def create
     ActiveRecord::Base.transaction do
       pre_order = current_user.pre_order
-      coupon = Coupon.find(order_params[:coupon_id].to_i)
+      coupon = order_params[:coupon].to_i
       @order = current_user.orders.create!(
         delivery_id: pre_order.delivery_id,
         payment_id: pre_order.payment_id,
         delivery_day: pre_order.hope_day,
         delivery_hour: pre_order.hope_hour,
         used_point: order_params[:used_point],
-        coupon_id: order_params[:coupon_id],
+        coupon: coupon,
         order_num: "#{current_user.id}_#{pre_order.id}",
-        total_price: current_user.cart.total_price + pre_order.delivery.price + pre_order.payment.price - order_params[:used_point].to_i - coupon.price)
+        total_price: current_user.cart.total_price + pre_order.delivery.price + pre_order.payment.price - order_params[:used_point].to_i - coupon)
       # 中間テーブルにアイテムを登録
       item_nums = current_user.cart.item_nums.group(:number)
       count = item_nums.count # 購入したアイテムの個数を算出
@@ -76,7 +77,7 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:coupon_id, :used_point)
+    params.require(:order).permit(:coupon, :used_point)
   end
   def setup_user
       @user = User.find(current_user.id)
