16ee5ef6c7f7436b90c72e2a0325b8958897e290
suguru_nakayama <croixdusudx@gmail.com>
2018-10-15 17:56:25 +0900

    クーポン追加に伴い注文確認画面の表示を行うJSを修正

--- app/assets/javascripts/point.js
+++ app/assets/javascripts/point.js
@@ -2,6 +2,7 @@ $(function(){
   $('.point-btn').on('click', function(){
     use_point = $(".point_form").val();
     current_point = Number($(".current_point").text())
+    coupon_point = Number($(".coupon_price").text().replace(",", "").match(/\d+/))
     item_price = Number($(".payment-total-num").text().replace(",", "").match(/\d+/)[0]);
 
     if (use_point.match(/[^0-9]+/) == null) {
@@ -19,7 +20,7 @@ $(function(){
         // 「合計欄」の表示を変更
         delivery_price = Number($(".payment-option-delivery-price").text().replace(",", "").match(/\d+/)[0]);
         payment_price = Number($(".payment-price").text().replace(",", "").match(/\d+/)[0]);
-        total_price = item_price + delivery_price + payment_price - use_point
+        total_price = item_price + delivery_price + payment_price - use_point - coupon_point
 
         final_price = "￥" + String(total_price).replace(/(\d)(?=(\d\d\d)+$)/g, "$1,");
         $(".payment-option-total-price").empty();
@@ -28,6 +29,8 @@ $(function(){
     }else {
       $(".point_form").val(0);
       alert("半角英数字を入力してください");
+      $(".coupon_price").empty();
+      $(".coupon_price").append(`<a>￥${coupon_point}</a>`);
     }
   })
 })
