bbef68d3e8213a7cc4e39f00c8e9834c44f98c7a
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-16 10:55:22 +0900

    Fix commit

--- app/assets/javascripts/enlargement.js
+++ app/assets/javascripts/enlargement.js
@@ -1,19 +1,26 @@
 $(function() {
   $(".main-image").on("mousemove", function(e){
     $(".zoom-area").addClass('active');
-    $(".image-grass").addClass('active');
+
     var size_left = e.pageX - 360;
-    var size_top = e.pageY - 860;
-    var left = size_left + 'px';
-    var top = size_top + 'px';
-    $(".image-grass").css("margin-left", left);
-    $(".image-grass").css("margin-top", top);
+    var size_top = e.pageY - 260;
+    // if (size_left < 300 && 0 < size_left && 0 < size_top && size_top < 400){
+      $(".image-grass").addClass('active');
+      var left = size_left + 'px';
+      var top = size_top + 'px';
+      $(".image-grass").css("left", left);
+      $(".image-grass").css("top", top);
+    // }
+
+
     var unleft = "-" + ((e.pageX - 260) * 2 ) + 'px';
     var untop = "-" + ((e.pageY - 160) * 2 ) + 'px';
     $( ".zoom_image" ).css( "margin-top" , untop );
     $( ".zoom_image" ).css( "margin-left" , unleft );
+
   });
-  $(".main-image").on("mouseout", function(){
+
+  $(".main-image").on("mouseleave", function(){
     $(".zoom-area").removeClass('active');
     $(".image-grass").removeClass('active');
   });
