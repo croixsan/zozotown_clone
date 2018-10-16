239a9bd73907efdfda668b00977718c04ee9f5b3
suguru_nakayama <croixdusudx@gmail.com>
2018-10-17 00:01:39 +0900

    アングルアイコンを押した時にサブイメージがボーダーで囲われるように変更

--- app/assets/javascripts/image_slide.js
+++ app/assets/javascripts/image_slide.js
@@ -1,4 +1,18 @@
 $(function(){
+  function replaceZoomImage(next_id, next_src){
+    $(".zoom-area").empty();
+    $(".zoom-area").append(`<img src=${next_src} class='zoom_image' id='${next_id}'>`);
+  }
+  function replaceTopImage(next_id, next_src){
+    $(".main-image").empty();
+    $(".main-image").append(`<img src=${next_src} class='top_image' id='${next_id}'>`);
+  }
+
+  function addBorder(next_id){
+    $(".sub_image").removeClass("selected_image")
+    $("#"+ next_id).addClass("selected_image");
+  }
+
   $(".sub_image").on("click", function(){
     image_src = $(this).attr("src");
     $(".main-image").empty();
@@ -8,28 +22,32 @@ $(function(){
   })
 
   $(".right-angle").on("click", function(){
-    current_id = Number($(".top_image").attr("id"))
+    var current_id = Number($(".top_image").attr("id"))
 
-    if (current_id < $(".sub-image-content li").length - 1){
-      next_id = current_id + 1;
+    if(current_id < $(".sub-image-content li").length - 1){
+      var next_id = current_id + 1;
     }else {
-      next_id = 0
+      var next_id = 0
     }
-    next_src = $("#" + String(next_id)).attr("src")
-    $(".main-image").empty();
-    $(".main-image").append(`<img src=${next_src} class='top_image' id='${next_id}'>`);
+    var next_src = $("#" + String(next_id)).attr("src")
+
+    addBorder(next_id) // サブイメージを枠線で囲う
+    replaceTopImage(next_id, next_src) //トップ画像を切り替える
+    replaceZoomImage(next_id, next_src); //拡大画像エリアを切り替える
   })
 
   $(".left-angle").on("click", function(){
     current_id = Number($(".top_image").attr("id"))
 
-    if (current_id > 0){
+    if(current_id > 0){
       next_id = current_id - 1;
     }else {
       next_id = $(".sub-image-content li").length - 1
     }
     next_src = $("#" + String(next_id)).attr("src")
-    $(".main-image").empty();
-    $(".main-image").append(`<img src=${next_src} class='top_image' id='${next_id}'>`);
+
+    addBorder(next_id) // サブイメージを枠線で囲う
+    replaceTopImage(next_id, next_src) //トップ画像を切り替える
+    replaceZoomImage(next_id, next_src); //拡大画像エリアを切り替える
   })
 })
