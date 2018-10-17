fca2fd922d868d6f808663f653a5033d206aeab4
suguru_nakayama <croixdusudx@gmail.com>
2018-10-17 20:36:28 +0900

    Userの所有ポイントにdefault: 0を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181015101435) do
+ActiveRecord::Schema.define(version: 20181017111431) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",        null: false
