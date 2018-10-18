a331ed0f2af156ffa7b091ec7c0dbe3fdbcb90fe
suguru_nakayama <croixdusudx@gmail.com>
2018-10-18 17:04:27 +0900

    privateメソッド化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
