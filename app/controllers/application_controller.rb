f628d67e3dc281169461a9445b8a57cecb8d8e65
suguru_nakayama <croixdusudx@gmail.com>
2018-10-18 18:01:15 +0900

    Edit aprctrl.rb2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,8 +27,7 @@ class ApplicationController < ActionController::Base
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      # username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
-      username == 'admin' && password == '2222'
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 
