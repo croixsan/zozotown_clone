b1638c51e6fd5325b14745a3ec5aa0a56ceea986
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-19 17:50:14 +0900

    Enlargement.js fix

--- app/assets/javascripts/enlargement.js
+++ app/assets/javascripts/enlargement.js
@@ -2,6 +2,15 @@ $(function() {
 
   var scale = 500 / 200;
 
+  $(".top_image").on('load', function(){
+    var image_left = $('.top_image').outerWidth();
+    var image_top = $('.top_image').outerHeight();
+    var grass_left = $('.image-grass').outerWidth();
+    var grass_top = $('.image-grass').outerHeight();
+    xmax = image_left - grass_left;
+    ymax = image_top - grass_top;
+  });
+
   $(".main-image").on("mouseenter", function(){
     var zoom_image = $('.top_image').outerWidth();
     zoom_image_size = zoom_image * scale;
@@ -17,20 +26,28 @@ $(function() {
 
   $(".main-image").on("mousemove", function(e){
     var image_offset = $('.main-image').offset();
-    var left = (e.pageX - image_offset.left - 100) ;
+    var left = (e.pageX - image_offset.left - 100)  ;
     var top = (e.pageY - image_offset.top - 100 ) ;
-    var px_left = (e.pageX - image_offset.left - 100) + 'px';
-    var px_top = (e.pageY - image_offset.top - 100 ) + 'px';
 
-    // var size_content = e.pageXOffset;
-    // if (size_left < 300 && 0 < size_left && 0 < size_top && size_top < 400){
-    $(".image-grass").css("left", px_left);
-    $(".image-grass").css("top", px_top);
-    // }
+    if(left > xmax){
+      left = xmax;
+    }
+    if(top > ymax){
+      top = ymax;
+    }
+    if(left < 0){
+      left = 0;
+    }
+    if(top < 0){
+      top = 0;
+    }
+
+    $(".image-grass").css("left", left);
+    $(".image-grass").css("top", top);
+
     var unleft = "-" + ( left * scale ) + 'px';
     var untop = "-" + ( top * scale ) + 'px';
     $( ".zoom_image" ).css( "margin-left" , unleft );
     $( ".zoom_image" ).css( "margin-top" , untop );
-
   });
 });
