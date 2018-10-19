3980e9a6d5a99eee641a933436d16eb7c4f34073
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-19 16:20:57 +0900

    Edit .image-grass coordinate

--- app/assets/javascripts/enlargement.js
+++ app/assets/javascripts/enlargement.js
@@ -14,27 +14,21 @@ $(function() {
   });
 
 
-  // $(".main-image").on("mousemove", function(e){
+  $(".main-image").on("mousemove", function(e){
+    var image_offset = $('.main-image').offset();
+    var size_left = (e.pageX - image_offset.left - 100) + 'px';
+    var size_top = (e.pageY - image_offset.top - 100 ) + 'px';
 
+    // var size_content = e.pageXOffset;
+    // if (size_left < 300 && 0 < size_left && 0 < size_top && size_top < 400){
+    $(".image-grass").css("left", size_left);
+    $(".image-grass").css("top", size_top);
+    // }
 
-  //   var size_left = e.pageX - 360;
-  //   var size_top = e.pageY - 260;
-  //   // if (size_left < 300 && 0 < size_left && 0 < size_top && size_top < 400){
-  //     var left = size_left + 'px';
-  //     var top = size_top + 'px';
-  //     $(".image-grass").css("left", left);
-  //     $(".image-grass").css("top", top);
-  //   // }
+    // var unleft = "-" + ((e.pageX - 260) * 2 ) + 'px';
+    // var untop = "-" + ((e.pageY - 160) * 2 ) + 'px';
+    // $( ".zoom_image" ).css( "margin-top" , untop );
+    // $( ".zoom_image" ).css( "margin-left" , unleft );
 
-  //   var unleft = "-" + ((e.pageX - 260) * 2 ) + 'px';
-  //   var untop = "-" + ((e.pageY - 160) * 2 ) + 'px';
-  //   $( ".zoom_image" ).css( "margin-top" , untop );
-  //   $( ".zoom_image" ).css( "margin-left" , unleft );
-
-  // });
-
-  // $(".main-image").on("mouseleave", function(){
-  //   $(".zoom-area").removeClass('active');
-  //   $(".image-grass").removeClass('active');
-  // });
+  });
 });
