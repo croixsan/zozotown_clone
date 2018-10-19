cbf59c08ef2b25520fa37ec9ff2ca00bfd4069c7
sotamatsumura <sota.matsumura0228@gmail.com>
2018-10-19 16:33:12 +0900

    Edit .zoom_image margin

--- app/assets/javascripts/enlargement.js
+++ app/assets/javascripts/enlargement.js
@@ -1,7 +1,8 @@
 $(function() {
 
+  var scale = 500 / 200;
+
   $(".main-image").on("mouseenter", function(){
-    var scale = 500 / 200;
     var zoom_image = $('.top_image').outerWidth();
     zoom_image_size = zoom_image * scale;
 
@@ -16,19 +17,20 @@ $(function() {
 
   $(".main-image").on("mousemove", function(e){
     var image_offset = $('.main-image').offset();
-    var size_left = (e.pageX - image_offset.left - 100) + 'px';
-    var size_top = (e.pageY - image_offset.top - 100 ) + 'px';
+    var left = (e.pageX - image_offset.left - 100) ;
+    var top = (e.pageY - image_offset.top - 100 ) ;
+    var px_left = (e.pageX - image_offset.left - 100) + 'px';
+    var px_top = (e.pageY - image_offset.top - 100 ) + 'px';
 
     // var size_content = e.pageXOffset;
     // if (size_left < 300 && 0 < size_left && 0 < size_top && size_top < 400){
-    $(".image-grass").css("left", size_left);
-    $(".image-grass").css("top", size_top);
+    $(".image-grass").css("left", px_left);
+    $(".image-grass").css("top", px_top);
     // }
-
-    // var unleft = "-" + ((e.pageX - 260) * 2 ) + 'px';
-    // var untop = "-" + ((e.pageY - 160) * 2 ) + 'px';
-    // $( ".zoom_image" ).css( "margin-top" , untop );
-    // $( ".zoom_image" ).css( "margin-left" , unleft );
+    var unleft = "-" + ( left * scale ) + 'px';
+    var untop = "-" + ( top * scale ) + 'px';
+    $( ".zoom_image" ).css( "margin-left" , unleft );
+    $( ".zoom_image" ).css( "margin-top" , untop );
 
   });
 });
